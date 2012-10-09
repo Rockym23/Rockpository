55a72fdceff1767a6760dbfde8a799f352eed9cb
Rocky <rockym0303@gmail.com>
2012-10-08 17:02:49 -0700

    Commitde inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rocky::Application.config.secret_token = '01b81b7451127d9b44f2b38e23b2d1f05660ff6994d0f92d32a7840e02380ee3b3dd308ad672b77c111ff90822236dd9f86e4a8e1e294383c2ca17b94e4fb990'
